d2e868131166dd4b340058e5ec10c423e24405c9
zaferkucuk <53651443+zaferkucuk@users.noreply.github.com>
2026-03-15 14:05:25 +0100

    fix(i18n): disable automatic locale detection, always default to German
    Browser Accept-Language header was overriding the default locale,
    causing English-language browsers to be redirected to /en instead of /de.
    Now soluty.io always defaults to German as the canonical language.

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,5 +2,6 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['de', 'en', 'tr'],
-  defaultLocale: 'de'
+  defaultLocale: 'de',
+  localeDetection: false
 });
